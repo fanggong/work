1b84f96ce6939d928373596cc92b163df311504a
Fang <15221009458@163.com>
2021-04-23 15:46:39 +0800

    to fit safari

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    var destination = new Date('2021-10-05 00:00:00').getTime() / 1000;
+    var destination = new Date('Oct 05, 2021 00:00:00').getTime() / 1000;
     // var destination = new Date().getTime() / 1000
 
     new FlipDown(destination, 'wedding', {theme: 'light'}).start();
